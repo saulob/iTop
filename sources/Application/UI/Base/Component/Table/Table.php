ac69cf86d18fa6ee162ccc0e165456b5ee827dfa
Benjamin Dalsass <benjamin.dalsass@combodo.com>
2022-10-28 10:27:05 +0200

    php generate-ui-block.php component Table

---
+++ sources/Application/UI/Base/Component/Table/Table.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2010-2022 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+namespace Combodo\iTop\Application\UI\Base\Component\Table;
+
+
+use Combodo\iTop\Application\UI\Base\UIBlock;
+
+/**
+ * Class Table
+ *
+ * @package Combodo\iTop\Application\UI\Base\Component\Table
+ */
+class Table extends UIBlock
+{
+	// Overloaded constants
+	public const BLOCK_CODE = 'ibo-table';
+	public const DEFAULT_HTML_TEMPLATE_REL_PATH = 'base/components/table/layout';
+	public const DEFAULT_JS_TEMPLATE_REL_PATH = 'base/components/table/layout';
+}
